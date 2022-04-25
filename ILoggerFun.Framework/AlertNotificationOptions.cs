5ba4f25b6d8a1f4e92c58b5b628940e49ca30678
Shai Cohen <55643999+shaicohen-dot-com@users.noreply.github.com>
2022-04-24 19:47:00 -0700

    Add project files.

---
+++ ILoggerFun.Framework/AlertNotificationOptions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ILoggerFun.Framework
+{
+
+    public class AlertNotification
+    {
+        private readonly ILogger<AlertNotification> _logger;
+        private readonly IOptionsSnapshot<AlertNotificationOptions> _options;
+        protected Timer Timer { get;}
+        public AlertNotification(IOptionsSnapshot<AlertNotificationOptions> options, ILogger<AlertNotification> logger) =>
+            (_logger, _options, Timer) = (logger, options, new Timer(options.Value.Interval));
+
+        public void Start()
+        {
+            Timer.Interval = _options.Value.Interval;
+            Timer.Elapsed += Timer_Elapsed;
+            Timer.Start();
+        }
+
+        public void Slower()
+        {
+            Timer.Interval += _options.Value.Step;
+        }
+
+        public void Faster()
+        {
+            if (Timer.Interval - _options.Value.Step <= 0)
+                Timer.Interval = 1;
+            else
+                Timer.Interval -= _options.Value.Step;
+        }
+
+        public void Flip()
+        {
+            Timer.Enabled = !Timer.Enabled;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _logger.LogInformation($"Elapsed {DateTime.Now}");
+        }
+    }
+
+    public class AlertNotificationOptions
+    {
+        public bool Enabled { get; set; }
+        public double Interval { get; set; }
+        public double Step{ get; set; }
+    }
+}
---
+++ ILoggerFun.Framework/Program.cs
@@ -0,0 +1,101 @@
+﻿using ILoggerFun.Framework;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ILoggerFun.Framework
+{
+    internal class Program
+    {
+        //static async Task Main(string[] args)
+        //{
+        //}
+        //public class Program
+        //{
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configuration) =>
+            {
+                configuration.Sources.Clear();
+
+                IHostEnvironment env = context.HostingEnvironment;
+
+                configuration
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+            })
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddDebug();
+            })
+            .ConfigureServices((hostBuilderContext, services) =>
+                services.Configure<AlertNotificationOptions>(
+                    hostBuilderContext.Configuration.GetSection(
+                        nameof(AlertNotificationOptions))))
+            .ConfigureServices((hostBuilderContext, services) =>
+                services.AddTransient<AlertNotification>())
+            .Build();
+
+            var service = host.Services
+                .GetService<AlertNotification>();
+
+
+            host.RunAsync();
+            service.Start();
+            WaitForAnyKeyPress(service);
+        }
+
+        private static void WaitForAnyKeyPress(AlertNotification notification)
+        {
+            ConsoleKey key;
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    //
+                }
+
+                // Key is available - read it
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.Spacebar)
+                    notification.Flip();
+                else if (key == ConsoleKey.MediaNext)
+                    notification.Faster();
+                else if (key == ConsoleKey.MediaPrevious)
+                    notification.Slower();
+
+            } while (key != ConsoleKey.Escape);
+        }
+
+    }
+}
+namespace Microsoft.Extensions.Hosting
+{
+public static class IHostingExtensions
+    {
+        public static IHostBuilder ConfigureIOptions(this IHostBuilder hostContext) =>
+    hostContext.ConfigureServices((hostBuilderContext, services) => services
+        .Configure<AlertNotificationOptions>(
+            hostBuilderContext.Configuration.GetSection(
+                nameof(AlertNotificationOptions))));
+
+
+        public static IHostBuilder ConfigureServices(this IHostBuilder hostContext) =>
+            hostContext.ConfigureServices((hostBuilderContext, services) => services
+                .AddTransient<AlertNotification>());
+    }
+}
